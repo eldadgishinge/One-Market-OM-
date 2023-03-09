2173cce5573523eee224d8665425dbf3b7c1bd49
Apple <apple@Apples-MacBook-Air.local>
2023-03-09 10:25:23 +0200

    first commit

---
+++ one_market.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+fruit= {'orange' : '100 Rwf -200 Rwf',
+        'Banana' : '100 Rwf - 150 Rwf',
+        'mango'  : '200 Rwf - 300 Rwf',
+        'melons' : '2500 Rwf - 3500 Rwf',
+        'apples' : '400 Rwf - 500 Rwf',
+        'Papayas': '700 Rwf - 1000 Rwf',
+        'Pineapples':'300 Rwf - 500 Rwf',
+        'Lemons' : '100 Rwf -300 Rwf'
