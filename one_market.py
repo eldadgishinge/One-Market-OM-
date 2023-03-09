6fc6627f6547f81a30a646e1dfec61f7079a8f4a
paulinemutuku <p.mutuku@alustudent.com>
2023-03-09 01:35:19 -0800

    clothes

--- one_market.py
+++ one_market.py
@@ -39,3 +39,14 @@ drinks= {'Inyange mango juice' : '800 Rwf -900 Rwf',
 
 for keys, values in sorted(drinks.items()):
     print("{}: {}".format(keys, values))
+
+#lists of clothes and tgheir prices
+clothes= {'T-shirts' : '2500 Rwf -5000 Rwf',
+        'Skirts' : '3000 Rwf - 6000 Rwf',
+        'Trousers'  : '8000 Rwf - 12000 Rwf',
+        'Shorts' : '3000 Rwf - 6000 Rwf',
+        'Jackets' : '5000 Rwf - 10000 Rwf',
+        'Dresses': '6000 Rwf - 15000 Rwf',
+        'Socks':'500 Rwf - 1500 Rwf',
+        'Sweaters' : '5000 Rwf - 8000 Rwf',
+        'Tights' : '2500 Rwf - 4000 Rwf' }
