31cbf0299264757715efeb7d57d9d59fcf82336e
Esther Umunyana <e.umunyana@alustudent.com>
2023-03-09 11:41:05 +0200

    The list of phones from different companies and their prices

--- one_market.py
+++ one_market.py
@@ -11,4 +11,23 @@ fruit= {'orange' : '100 Rwf -200 Rwf',
         'Lemons' : '100 Rwf -300 Rwf'
 
 for keys, values in sorted(fruit.items()):
-    print("{}: {}".format(keys, values))
+print("{}: {}".format(keys, values))
+
+#This is a list of phones from different companies and their prices
+phones= {'iphones:',
+         'iphone 14 pro max (128GB 6GB RAM)' : '1M Rwf',
+         'iphone 14 pro (128GB 6GB RAM)' : '1.2M Rwf',
+         'iphone 14 plus (128GB 6GB RAM)' : '783571 RWf',
+         'iphone 13 pro max (128GB 6GB RAM)' : '892551 Rwf',
+         'iphone 13 pro (128GB 6GB RAM)' : '972107 Rwf',
+         'iphone 13 (128GB 6GB RAM)' : '799918 Rwf',
+         'iphone 13 mini (128GB 4GB RAM)' : '730170 Rwf',
+         '/n',
+         'Samsung:',
+         'Samsung S23 Ultra (256GB 8GB RAM)' : '1.1M Rwf',
+         'Samsung Galaxy S23 (128GB 8GB RAM)' : '811906 Rwf',
+         'Samsung A53 5GB (128GB 4GB RAM)' : '489323 Rwf',
+         'Samsung Galaxy S10 (128GB 8GB RAM)' : '391240 Rwf',
+         'Samsung Galaxy A13 (32GB 3GB RAM)' : '158022 Rwf',
+         '/n',
+         'More products comming soon...'
