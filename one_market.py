ec38f6ed7901afbcfcc0f4a22d3729ed2b1431f1
Azeezmariam <m.azeez@alustudent.com>
2023-03-09 01:07:26 -0800

    one_market.py

--- one_market.py
+++ one_market.py
@@ -11,4 +11,14 @@ fruit= {'orange' : '100 Rwf -200 Rwf',
         'Lemons' : '100 Rwf -300 Rwf'
 
 for keys, values in sorted(fruit.items()):
-    print("{}: {}".format(keys, values))
+    print("{}: {}".format(keys, values)
+
+vegetables = {'Pumpkin': '300 Rwf - 400 Rwf',
+         'Tomato': '150 Rwf - 180 Rwf',
+         'Eggplant': '250 Rwf - 400 Rwf',
+         'Green peas': '1500 Rwf - 2000 Rwf',
+         'Corn': '400 Rwf - 500 Rwf',
+         'Garlic': '100 Rwf - 200 Rwf',
+         'Cucumber': '200 Rwf - 300 Rwf',
+         'Onions': '100 Rwf -200 Rwf'
+        }
